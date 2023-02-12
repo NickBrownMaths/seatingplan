b27addc952e1be38466c73dfaa4807d57d3a9e1f
Nicholas Brown <nbrown@softwareinstitute.com>
2023-02-12 00:15:16 +0000

    added instruction page

--- src/PageBodyDecider.js
+++ src/PageBodyDecider.js
@@ -16,5 +16,46 @@ function PageBodyDecider(props) {
       />
     )
   }
+  else if (props.currentPage === 'instruct') {
+    return (
+
+      <div className="LoadSaveHolder" >
+        <div className="InstructionBox">
+          This app helps party planners assign seats at tables.
+          Click the Assign Button to start.
+          Groups have a number of bums (B), and a number of kids (K).
+          As groups are added to tables, the tables will to the total number of Bs and Ks who are to be seated there.
+          <br />
+          <br />
+          <button className='FakeKillButton' > X </button>
+          <button className='FakeLinkButton' > @ </button>
+          <button className='FakeUnlinkButton' > Ø </button>
+          <br />
+          Clicking the red X will remove a table or group.
+          Clicking a green @ on a table, and another on a group, assigns that group to that table.
+          Clicking the yellow Ø will unassign a group.
+        </div>
+      </div>
+    )
+  }
+  else if (props.currentPage === 'saveSesh') {
+    return (
+      <div className="LoadSaveHolder" >
+        <div className="InstructionBox">
+          Copy the box below and save it to a text file.
+        </div>
+        <br />
+        <div className="CopyBox">
+          {"{"}
+          "TABLELIST": {JSON.stringify(props.tableList)},
+          "PARTYLIST": {JSON.stringify(props.partyList)}
+          {"}"}
+        </div>
+      </div>
+    )
+  }
+  else {
+
+  }
 }
 export default PageBodyDecider;
